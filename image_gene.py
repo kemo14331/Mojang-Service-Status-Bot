c23b2b92ff8e3da6bce85522311f72d12ca73aab
kemo431 <kemo14331@gmail.com>
2020-11-25 22:44:35 +0900

    スッキリ

--- image_gene.py
+++ image_gene.py
@@ -1,4 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
+import time
+from datetime import datetime
 
 font_size = 30
 white = (255, 255, 255)
@@ -7,6 +9,10 @@ status_colors = {
     "yellow": (255, 200, 0),
     "red": (240, 0, 0)
 }
+
+def get_timestamp():
+    timestamp = datetime.utcnow()
+    return str(timestamp.strftime("%Y/%m/%d %H:%M")) + "(UTC)"
 
 def make_image(status: dict):
     img = Image.new("RGB", (1500, 500), (40,40,45))
@@ -28,5 +34,8 @@ def make_image(status: dict):
         draw.ellipse((ex, y + 5, ex + font_size, y + font_size + 5), fill=status_colors[status[service]])
         draw.text((tx + 10, y), service, fill=white, font = font)
         y += font_size + 15
+    
+    width, height = draw.textsize(f"Last Update: {get_timestamp()}", font=font)
+    draw.text((1500 - width - 5, 500 - height - 5), f"Last Update: {get_timestamp()}", fill=white, font=font)
 
     img.save("./tmp/tmp.png")
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os
 import time
-import glob
-import base64
 from datetime import datetime
 from pprint import pprint
 
-import emoji
 import schedule
 import tweepy
 from mojang import MojangAPI
@@ -24,19 +21,10 @@ rebooted = True
 service_status_list = {}
 waiting_send_list = {}
 
-auth = tweepy.OAuthHandler(CK, CS)
-auth.set_access_token(AT, AS)
-api = tweepy.API(auth)
-
-if not os.path.exists("./tmp"):
-    os.mkdir("./tmp")
-
-print("api connected")
-
 
 def get_timestamp():
-    timestamp = datetime.today()
-    return str(timestamp.strftime("%Y/%m/%d %H:%M"))
+    timestamp = datetime.utcnow()
+    return str(timestamp.strftime("%Y/%m/%d %H:%M")) + "UTC"
 
 
 def tweet_services_status(status_changed_services: dict):
@@ -56,88 +44,60 @@ def tweet_services_status(status_changed_services: dict):
 
     for service in list(waiting_send_list.keys()):
         if waiting_send_list[service].get_elapsed_time().total_seconds() >= wait_time:
-            if waiting_send_list[service].status == "green":
-                online_services.append(service)
-            elif waiting_send_list[service].status == "red":
+            if waiting_send_list[service].status == "red":
                 unavailable_services.append(service)
+            else:
+                online_services.append(service)
             del waiting_send_list[service]
 
     if len(status_changed_services.keys()) > 0:
-        print("===detected status of service changed===")
+        print("detected status of service changed.")
         print("waiting_send_list:")
         pprint(waiting_send_list)
-        print("========================================")
 
     if len(online_services) > 0:
-        tweet_online_services(online_services)
+        tweet_services(online_services, ONLINE)
 
     if len(unavailable_services) > 0:
-        tweet_unavailable_services(unavailable_services)
+        tweet_services(unavailable_services, UNAVAILABLE)
 
 
-def tweet_online_services(services: list):
-    print("tweet online services.")
-    msg = "✅The service has returned to normal.\n"
-
-    if len(services) == 1:
-        msg += "Service:\n"
-    else:
-        msg += "Services:\n"
-
-    for service in services:
-        msg += service + '\n'
-
+def get_services_msg(services: list):
+    msg = "Service:\n" if len(services) == 1 else "Services:\n"
+    msg.join(list(map(lambda service: service + "\n", services)))
     msg += "\n" + get_timestamp()
+
+
+ONLINE, UNAVAILABLE = 0, 1
+def tweet_services(services: list, mode: int):
+    msg = ""
+    if mode == ONLINE:
+        msg += "✅The service has returned to normal.\n"
+    elif mode == UNAVAILABLE:
+        msg += "❌The service has be currently unavailable.\n"
+    msg += get_services_msg(services)
     try:
         api.update_status(status=msg)
     except Exception as e:
         print(e)
 
-
-def tweet_unavailable_services(services: list):
-    print("tweet unavailable services.")
-    msg = "❌The service has be currently unavailable.\n"
-
-    if len(services) == 1:
-        msg += "Service:\n"
-    else:
-        msg += "Services:\n"
-
-    for service in services:
-        msg += service + '\n'
-    msg += "\n" + get_timestamp()
-    try:
-        api.update_status(status=msg)
-    except Exception as e:
-        print(e)
 
 def update_profile(status: dict):
-    msg = ""
-    msg += "Services Status:\n"
-    show_list = (
-        "minecraft.net",
-        "session.minecraft.net",
-        "account.mojang.com",
-        "sessionserver.mojang.com",
-        "authserver.mojang.com",
-        "textures.minecraft.net"
-    )
-    for service in status.keys():
-        if service in show_list:
-            if status[service] == "green":
-                msg += f"😃{service}\n"
-            elif status[service] == "yellow":
-                msg += f"😰{service}\n"
-            elif status[service] == "red":
-                msg += f"💀{service}\n"
     try:
         print("update profile")
         make_image(status)
         api.update_profile_banner(filename="./tmp/tmp.png")
-        #api.update_profile(description=msg)
     except Exception as e:
         print(e)
-        pprint(msg.rstrip())
+
+
+def is_status_should_notify(status: str, service: str):
+    global service_status_list
+    if service_status_list[service] == "red":
+        return True
+    elif status[service] == "red":
+        return True
+    return False
 
 
 def task():
@@ -152,11 +112,10 @@ def task():
             update_profile(status)
             rebooted = False
         else:
-            status_changed_services = {}
             for service in status.keys():
-                if(service_status_list[service] != status[service]):
+                if service_status_list[service] != status[service]:
                     service_status_change = True
-                    if(status[service] == "green" or status[service] == "red"):
+                    if is_status_should_notify(status[service], service):
                         status_changed_services[service] = ServiceState(
                             status=status[service], last_changed_time=datetime.now())
 
@@ -170,8 +129,17 @@ def task():
         pass
 
 
-schedule.every(2).seconds.do(task)
+if __name__ == "__main__":
+    auth = tweepy.OAuthHandler(CK, CS)
+    auth.set_access_token(AT, AS)
+    api = tweepy.API(auth)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    if not os.path.exists("./tmp"):
+        os.mkdir("./tmp")
+
+    print("api connected")
+    schedule.every(2).seconds.do(task)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
